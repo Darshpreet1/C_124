a494f64500a701eee270b6ac737215342bf833ff
Darshpreet1 <102862814+Darshpreet1@users.noreply.github.com>
2022-10-28 19:28:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
-    function preload(){
+function preload(){
 
+}
+
+function setup(){
+    Canvas = createCanvas(300,300);
+    Canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide()
+
+    poseNet = ml5.poseNet(video,modelLoded);
+    poseNet.on("pose",Gotposes);
+}
+
+function draw(){
+  image(video,0,0,300,300);
+}
+
+function take_snapshot(){
+    save("Myfilterimg.jpg");
+}
+
+function modelLoded(){
+    console.log("model Loded");
+}
+
+function Gotposes(resultes)
+{
+    if(resultes.length >0 ){
+        console.log( resultes );
+        console.log( "nose x = "+ resultes[0].pose.nose.x);
+        console.log( "nose y = "+ resultes[0].pose.nose.y);
     }
-    
-    function setup(){
-        Canvas = createCanvas(300,300);
-        Canvas.center();
-    }
-    
-    function draw(){
-    
-    }
-    
-    function take_snapshot(){
-        save("filtered_image.jpg");
-    }
+}
